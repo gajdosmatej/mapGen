70108ad8b3d708f8d30c1cf20dcd29942513d968
Matěj Gajdoš <matej.gajdos@cvut.cz>
2023-06-28 11:57:59 +0200

    GUI.py ... Added background colour argument to _plotTile_ method.

--- GUI.py
+++ GUI.py
@@ -12,14 +12,15 @@ class WindowHandler:
 		self.canvas.pack()
 
 		self.tile_side_length = 50
-		self.plotTile(200,150)
+		self.plotTile(200,150, "#006611")
 		tkinter.mainloop()
 
-	def plotTile(self, x_pos, y_pos):
+	def plotTile(self, x_pos, y_pos, background_colour):
 		'''
 		Plot hexagon tile to the WindowHandler's canvas.
 		@x_pos: Pixel x position of the hexagon centre.
 		@y_pos: Pixel y position of the hexagon centre.
+		@background_colour: Fill colour of the tile.
 		'''
 		points = [	x_pos, y_pos-self.tile_side_length,
 					x_pos+0.866*self.tile_side_length, y_pos-0.5*self.tile_side_length,
@@ -27,5 +28,5 @@ class WindowHandler:
 					x_pos, y_pos+self.tile_side_length,
 					x_pos-0.866*self.tile_side_length, y_pos+0.5*self.tile_side_length,
 					x_pos-0.866*self.tile_side_length, y_pos-0.5*self.tile_side_length]
-		self.canvas.create_polygon(points, outline='black', fill='gray', width=2)
+		self.canvas.create_polygon(points, outline='black', fill=background_colour, width=2)
 		
